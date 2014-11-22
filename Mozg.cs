ad6a72bb6243b9abf40c8ca81b5d07195069b436
UncleAlex <apiary77@mail.ru>
2014-11-22 12:20:39 +0700

    mozg

---
+++ Mozg.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGIT
+{
+	class Mozg
+	{
+		private int level;
+		private int count;
+	}
+}
